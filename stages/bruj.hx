b16c332dda06545c87ec2766233cb5d9d388139e
TheLeerName <lesha.nechaev.05@mail.ru>
2023-12-08 12:55:36 +0700

    return of gf

--- stages/bruj.hx
+++ stages/bruj.hx
@@ -1,5 +1,6 @@
 var bg = new FlxSprite(-330, -250).loadGraphic(Paths.image('BGbruj'));
 bg.scale.set(1.75, 1.75);
+bg.scrollFactor.set(0.95, 0.95);
 addBehindGF(bg);
 
 game.initHScript(Paths.modFolders("cameramovesonnotehit.hx"));
--- stages/subway.hx
+++ stages/subway.hx
@@ -1,5 +1,6 @@
 var bg = new FlxSprite(-300, -300).loadGraphic(Paths.image('BGsubway'));
 bg.scale.set(2, 2);
+bg.scrollFactor.set(0.95, 0.95);
 addBehindGF(bg);
 
 game.initHScript(Paths.modFolders("cameramovesonnotehit.hx"));
